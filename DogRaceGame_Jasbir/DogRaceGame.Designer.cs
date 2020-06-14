841733b6e181c67dcd5e825699a8549d6f0b1f3a
Sukhman Singh <SS113@atcltd.atc.org.nz>
2020-06-15 10:02:28 +1200

    Add project files.

---
+++ DogRaceGame_Jasbir/DogRaceGame.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace DogRaceGame_Jasbir
+{
+    partial class DogRaceGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dog2 = new System.Windows.Forms.PictureBox();
+            this.dog1 = new System.Windows.Forms.PictureBox();
+            this.dog4 = new System.Windows.Forms.PictureBox();
+            this.dog3 = new System.Windows.Forms.PictureBox();
+            this.dogRaceTrack = new System.Windows.Forms.PictureBox();
+            this.bettingParlor = new System.Windows.Forms.GroupBox();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.raceBtn = new System.Windows.Forms.Button();
+            this.numericUpDownNumber = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDownForBet = new System.Windows.Forms.NumericUpDown();
+            this.betsBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sandeepBetLabel = new System.Windows.Forms.Label();
+            this.rajinderBetLabel = new System.Windows.Forms.Label();
+            this.tejbirBetLabel = new System.Windows.Forms.Label();
+            this.radioButtonSandeep = new System.Windows.Forms.RadioButton();
+            this.radioButtonRajinder = new System.Windows.Forms.RadioButton();
+            this.radioButtonJasbir = new System.Windows.Forms.RadioButton();
+            this.minimumBetLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dog2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dog1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dog4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dog3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dogRaceTrack)).BeginInit();
+            this.bettingParlor.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownNumber)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownForBet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dog2
+            // 
+            this.dog2.Image = global::DogRaceGame_Jasbir.Properties.Resources.dog21;
+            this.dog2.Location = new System.Drawing.Point(31, 128);
+            this.dog2.Name = "dog2";
+            this.dog2.Size = new System.Drawing.Size(100, 50);
+            this.dog2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dog2.TabIndex = 6;
+            this.dog2.TabStop = false;
+            // 
+            // dog1
+            // 
+            this.dog1.Image = global::DogRaceGame_Jasbir.Properties.Resources.dog11;
+            this.dog1.Location = new System.Drawing.Point(31, 44);
+            this.dog1.Name = "dog1";
+            this.dog1.Size = new System.Drawing.Size(100, 50);
+            this.dog1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dog1.TabIndex = 5;
+            this.dog1.TabStop = false;
+            // 
+            // dog4
+            // 
+            this.dog4.Image = global::DogRaceGame_Jasbir.Properties.Resources.dog4;
+            this.dog4.Location = new System.Drawing.Point(31, 303);
+            this.dog4.Name = "dog4";
+            this.dog4.Size = new System.Drawing.Size(100, 50);
+            this.dog4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dog4.TabIndex = 4;
+            this.dog4.TabStop = false;
+            // 
+            // dog3
+            // 
+            this.dog3.Image = global::DogRaceGame_Jasbir.Properties.Resources.dog3;
+            this.dog3.Location = new System.Drawing.Point(31, 217);
+            this.dog3.Name = "dog3";
+            this.dog3.Size = new System.Drawing.Size(100, 50);
+            this.dog3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dog3.TabIndex = 3;
+            this.dog3.TabStop = false;
+            // 
+            // dogRaceTrack
+            // 
+            this.dogRaceTrack.Image = global::DogRaceGame_Jasbir.Properties.Resources.track;
+            this.dogRaceTrack.Location = new System.Drawing.Point(0, 0);
+            this.dogRaceTrack.Name = "dogRaceTrack";
+            this.dogRaceTrack.Size = new System.Drawing.Size(900, 400);
+            this.dogRaceTrack.TabIndex = 0;
+            this.dogRaceTrack.TabStop = false;
+            // 
+            // bettingParlor
+            // 
+            this.bettingParlor.BackColor = System.Drawing.Color.Gainsboro;
+            this.bettingParlor.Controls.Add(this.resetBtn);
+            this.bettingParlor.Controls.Add(this.raceBtn);
+            this.bettingParlor.Controls.Add(this.numericUpDownNumber);
+            this.bettingParlor.Controls.Add(this.label3);
+            this.bettingParlor.Controls.Add(this.numericUpDownForBet);
+            this.bettingParlor.Controls.Add(this.betsBtn);
+            this.bettingParlor.Controls.Add(this.label2);
+            this.bettingParlor.Controls.Add(this.sandeepBetLabel);
+            this.bettingParlor.Controls.Add(this.rajinderBetLabel);
+            this.bettingParlor.Controls.Add(this.tejbirBetLabel);
+            this.bettingParlor.Controls.Add(this.radioButtonSandeep);
+            this.bettingParlor.Controls.Add(this.radioButtonRajinder);
+            this.bettingParlor.Controls.Add(this.radioButtonJasbir);
+            this.bettingParlor.Controls.Add(this.minimumBetLabel);
+            this.bettingParlor.Location = new System.Drawing.Point(0, 406);
+            this.bettingParlor.Name = "bettingParlor";
+            this.bettingParlor.Size = new System.Drawing.Size(913, 217);
+            this.bettingParlor.TabIndex = 7;
+            this.bettingParlor.TabStop = false;
+            this.bettingParlor.Text = "Betting Place";
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.BackColor = System.Drawing.Color.Red;
+            this.resetBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.resetBtn.Location = new System.Drawing.Point(774, 34);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(112, 122);
+            this.resetBtn.TabIndex = 23;
+            this.resetBtn.Text = "Reset";
+            this.resetBtn.UseVisualStyleBackColor = false;
+            this.resetBtn.Click += new System.EventHandler(this.resetBtn_Click);
+            // 
+            // raceBtn
+            // 
+            this.raceBtn.BackColor = System.Drawing.Color.Red;
+            this.raceBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.raceBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.raceBtn.Location = new System.Drawing.Point(517, 34);
+            this.raceBtn.Name = "raceBtn";
+            this.raceBtn.Size = new System.Drawing.Size(237, 122);
+            this.raceBtn.TabIndex = 22;
+            this.raceBtn.Text = "Race!";
+            this.raceBtn.UseVisualStyleBackColor = false;
+            this.raceBtn.Click += new System.EventHandler(this.raceBtn_Click);
+            // 
+            // numericUpDownNumber
+            // 
+            this.numericUpDownNumber.Location = new System.Drawing.Point(286, 170);
+            this.numericUpDownNumber.Maximum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numericUpDownNumber.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownNumber.Name = "numericUpDownNumber";
+            this.numericUpDownNumber.Size = new System.Drawing.Size(45, 20);
+            this.numericUpDownNumber.TabIndex = 21;
+            this.numericUpDownNumber.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(173, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "bets on truck number";
+            // 
+            // numericUpDownForBet
+            // 
+            this.numericUpDownForBet.Location = new System.Drawing.Point(117, 172);
+            this.numericUpDownForBet.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownForBet.Name = "numericUpDownForBet";
+            this.numericUpDownForBet.Size = new System.Drawing.Size(50, 20);
+            this.numericUpDownForBet.TabIndex = 19;
+            this.numericUpDownForBet.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // betsBtn
+            // 
+            this.betsBtn.BackColor = System.Drawing.Color.Red;
+            this.betsBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.betsBtn.Location = new System.Drawing.Point(31, 170);
+            this.betsBtn.Name = "betsBtn";
+            this.betsBtn.Size = new System.Drawing.Size(76, 23);
+            this.betsBtn.TabIndex = 18;
+            this.betsBtn.Text = "Bets";
+            this.betsBtn.UseVisualStyleBackColor = false;
+            this.betsBtn.Click += new System.EventHandler(this.betsBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(217, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 16);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Bets";
+            // 
+            // sandeepBetLabel
+            // 
+            this.sandeepBetLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.sandeepBetLabel.Location = new System.Drawing.Point(220, 127);
+            this.sandeepBetLabel.Name = "sandeepBetLabel";
+            this.sandeepBetLabel.Size = new System.Drawing.Size(197, 17);
+            this.sandeepBetLabel.TabIndex = 15;
+            this.sandeepBetLabel.Text = "SandeepBetLabel";
+            // 
+            // rajinderBetLabel
+            // 
+            this.rajinderBetLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rajinderBetLabel.Location = new System.Drawing.Point(220, 94);
+            this.rajinderBetLabel.Name = "rajinderBetLabel";
+            this.rajinderBetLabel.Size = new System.Drawing.Size(197, 17);
+            this.rajinderBetLabel.TabIndex = 14;
+            this.rajinderBetLabel.Text = "RajinderBetLabel";
+            // 
+            // tejbirBetLabel
+            // 
+            this.tejbirBetLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tejbirBetLabel.Location = new System.Drawing.Point(220, 62);
+            this.tejbirBetLabel.Name = "tejbirBetLabel";
+            this.tejbirBetLabel.Size = new System.Drawing.Size(197, 17);
+            this.tejbirBetLabel.TabIndex = 13;
+            this.tejbirBetLabel.Text = "JasbirBetLabel";
+            // 
+            // radioButtonSandeep
+            // 
+            this.radioButtonSandeep.AutoSize = true;
+            this.radioButtonSandeep.Location = new System.Drawing.Point(31, 126);
+            this.radioButtonSandeep.Name = "radioButtonSandeep";
+            this.radioButtonSandeep.Size = new System.Drawing.Size(122, 17);
+            this.radioButtonSandeep.TabIndex = 3;
+            this.radioButtonSandeep.TabStop = true;
+            this.radioButtonSandeep.Text = "radioButtonSandeep";
+            this.radioButtonSandeep.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonRajinder
+            // 
+            this.radioButtonRajinder.AutoSize = true;
+            this.radioButtonRajinder.Location = new System.Drawing.Point(31, 93);
+            this.radioButtonRajinder.Name = "radioButtonRajinder";
+            this.radioButtonRajinder.Size = new System.Drawing.Size(118, 17);
+            this.radioButtonRajinder.TabIndex = 2;
+            this.radioButtonRajinder.TabStop = true;
+            this.radioButtonRajinder.Text = "radioButtonRajinder";
+            this.radioButtonRajinder.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonJasbir
+            // 
+            this.radioButtonJasbir.AutoSize = true;
+            this.radioButtonJasbir.Location = new System.Drawing.Point(31, 61);
+            this.radioButtonJasbir.Name = "radioButtonJasbir";
+            this.radioButtonJasbir.Size = new System.Drawing.Size(106, 17);
+            this.radioButtonJasbir.TabIndex = 1;
+            this.radioButtonJasbir.TabStop = true;
+            this.radioButtonJasbir.Text = "radioButtonJasbir";
+            this.radioButtonJasbir.UseVisualStyleBackColor = true;
+            // 
+            // minimumBetLabel
+            // 
+            this.minimumBetLabel.AutoSize = true;
+            this.minimumBetLabel.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minimumBetLabel.Location = new System.Drawing.Point(28, 32);
+            this.minimumBetLabel.Name = "minimumBetLabel";
+            this.minimumBetLabel.Size = new System.Drawing.Size(83, 16);
+            this.minimumBetLabel.TabIndex = 0;
+            this.minimumBetLabel.Text = "Minimum Bet";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // DogRaceGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(917, 642);
+            this.Controls.Add(this.bettingParlor);
+            this.Controls.Add(this.dog2);
+            this.Controls.Add(this.dog1);
+            this.Controls.Add(this.dog4);
+            this.Controls.Add(this.dog3);
+            this.Controls.Add(this.dogRaceTrack);
+            this.Name = "DogRaceGame";
+            this.Text = "Dog Race";
+            ((System.ComponentModel.ISupportInitialize)(this.dog2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dog1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dog4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dog3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dogRaceTrack)).EndInit();
+            this.bettingParlor.ResumeLayout(false);
+            this.bettingParlor.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownNumber)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownForBet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox dogRaceTrack;
+        private System.Windows.Forms.PictureBox dog3;
+        private System.Windows.Forms.PictureBox dog4;
+        private System.Windows.Forms.PictureBox dog1;
+        private System.Windows.Forms.PictureBox dog2;
+        private System.Windows.Forms.GroupBox bettingParlor;
+        private System.Windows.Forms.Button resetBtn;
+        private System.Windows.Forms.Button raceBtn;
+        private System.Windows.Forms.NumericUpDown numericUpDownNumber;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDownForBet;
+        private System.Windows.Forms.Button betsBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label sandeepBetLabel;
+        private System.Windows.Forms.Label rajinderBetLabel;
+        private System.Windows.Forms.Label tejbirBetLabel;
+        private System.Windows.Forms.RadioButton radioButtonSandeep;
+        private System.Windows.Forms.RadioButton radioButtonRajinder;
+        private System.Windows.Forms.RadioButton radioButtonJasbir;
+        private System.Windows.Forms.Label minimumBetLabel;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
